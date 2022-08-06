4ce0b811ba4aafaef16247732745573cd7cd1bed
SSPENELOPE <typoepping@gmail.com>
2022-08-06 10:51:23 -0400

    updated fetch function

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 /*          Variables            */
 var searchBtn = document.getElementById("search");
-var cityList = document.getElementById("city-list")
+var cityList = document.getElementById("city-list");
+var cityInput = document.getElementById("city");
 
 
 
@@ -11,20 +12,24 @@ var cityList = document.getElementById("city-list")
 
 
 
+function getWeatherData() {
+    var weatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=d56a5eb4cf852a09ec80d61b85870176&';
 
-function getApi()
-    var requestUrl = "https://openweathermap.org/api/one-call-api";
-    fetch(requestUrl)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-    });
+    fetch(weatherUrl)
+        .then(function (response) {
+            if (!response.ok) {
+                alert("Error: " + response.statusText);
+            } else {
+                return response.json();
+            }
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+    };
 
 
 
-    searchBtn.addEventListener("click", function () {
-        getApi(); 
-    });
-    
+searchBtn.addEventListener("click", function () {
+    getWeatherData();
+});
